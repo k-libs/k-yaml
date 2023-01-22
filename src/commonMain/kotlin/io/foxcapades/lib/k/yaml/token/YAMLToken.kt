558bcf260c11ff1b95c9582adb0ac1163524bc22
Elizabeth <foxcapade@gmail.com>
2023-01-22 10:58:03 -0500

    updates

--- src/commonMain/kotlin/io/foxcapades/lib/k/yaml/token/YAMLToken.kt
+++ src/commonMain/kotlin/io/foxcapades/lib/k/yaml/token/YAMLToken.kt
@@ -9,6 +9,25 @@ sealed interface YAMLToken {
   val warnings: Array<SourceWarning>
 }
 
+class YAMLTokenDocumentStart(
+  override val start: SourcePosition,
+  override val end: SourcePosition,
+  override val warnings: Array<SourceWarning>
+) : YAMLToken {
+  override fun toString() =
+    "YAMLTokenDocumentStart(" +
+      "start=$start" +
+      ", " +
+      "end=$end" +
+      ", " +
+      "warnings=${warnings.joinToString(", ", "[", "]")}" +
+      ")"
+
+  override fun equals(other: Any?) =
+    this === other
+
+}
+
 class YAMLTokenz(
   val type: YAMLTokenType,
   val data: YAMLTokenData?,
